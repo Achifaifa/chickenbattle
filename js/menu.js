14b34e5738644bcb53d1ebc8af9cd55a1810e662
Yawin <tuzmakel@gmail.com>
2015-08-10 22:52:12 +0200

    En mainMenu() añadido fondo de menú

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,16 @@ function mainMenu()
 {
 	var fondo = new Image();
 	fondo.src = 'img/fondo.jpg';
+	
+	imgancho=ctx.canvas.width;
+	imgalto=(imgancho*720)/1280;
+	imgx=0;
+	imgy=-imgalto/2
+
 	fondo.onload = function()
 	{
-		ctx.drawImage(fondo,0,0,800,600);
+		ctx.drawImage(fondo,0,-imgalto/4,imgancho,imgalto);
 	}
+
+	
 }
