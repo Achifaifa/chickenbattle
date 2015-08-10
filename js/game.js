6aaf5f6f20ae759de9dc4e8e65666291a3ed984c
Yuri Numerov <achi@hush.ai>
2015-08-10 21:58:05 +0200

    fullscreen

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 var c=document.getElementById("game");
 c.style.background="#000000";
 var ctx=c.getContext("2d");
-ctx.canvas.width=800;
-ctx.canvas.height=600;
+ctx.canvas.width=window.innerWidth;
+ctx.canvas.height=window.innerHeight;
 ctx.fillStyle="white"
 ctx.font="15px sans-serif";
 
