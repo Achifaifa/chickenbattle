5cff5d65fb3e7b3d62ea25431fe07a87217b66e7
Yawin <tuzmakel@gmail.com>
2015-08-11 14:14:35 +0200

    Cambios en menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,10 +6,23 @@ function mainMenu()
 	imgancho=ctx.canvas.width;
 	imgalto=(imgancho*720)/1280;
 	imgx=0;
-	imgy=-imgalto/2;
+
+	if(ctx.canvas.height<imgalto)
+	{
+		imgy=-(imgalto-ctx.canvas.height)/2;
+	}
+	else if(ctx.canvas.height>imgalto)
+	{
+		imgy=imgalto/4;
+	}
+	else
+	{
+		imgy=0;
+	}
+	console.log(ctx.canvas.height+" "+imgalto+" "+imgy);
 
 	fondo.onload = function()
 	{
-		ctx.drawImage(fondo,0,-imgalto/4,imgancho,imgalto);
+		ctx.drawImage(fondo,0,imgy,imgancho,imgalto);
 	}
 }
