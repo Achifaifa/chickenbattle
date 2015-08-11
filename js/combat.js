258ef5f849bca18ea642e1ee019eaeb55cbc6140
Yuri Numerov <achi@hush.ai>
2015-08-11 06:41:20 +0200

    fixed layout, added test background

--- js/combat.js
+++ js/combat.js
@@ -1,3 +1,14 @@
+fightbg=new Image()
+fightbg.src="./img/fight_background.jpg"
+
+
+function background(ctx){
+  ctx.fillStyle=ctx.createPattern(fightbg, 'repeat');
+  // ctx.image(fightbg,0,0)
+  ctx.fillRect(0,0,window.innerWidth,window.innerHeight)
+  ctx.fillStyle="#AAAAAA"
+}
+
 function gui(ctx,w){
   /*
   Draws the combat layout (Health bars, timer, etc)
@@ -7,9 +18,9 @@ function gui(ctx,w){
   */
 
   //Temporary. The combat renderer should clear the screen first
-  ctx.fillStyle="black"
-  ctx.fillRect(0,0,window.innerWidth,window.innerHeight)
-  ctx.fillStyle="white"
+  // ctx.fillStyle="black"
+  // ctx.fillRect(0,0,window.innerWidth,window.innerHeight)
+  // ctx.fillStyle="#AAAAAA"
 
   // Calculate reference points
   var middle=window.innerWidth/2
@@ -17,20 +28,23 @@ function gui(ctx,w){
   // Draw time counter
   var timeleft=('00'+w.roundtime).slice(-2);
   ctx.font="60px sans-serif";
-  ctx.fillText(timeleft,middle-39,100)
+  ctx.fillText(timeleft,middle-39,75)
 
   // Draw life bars
-  var perc1=w.p1.life*100/w.p1.maxlife
-  var perc2=w.p2.life*100/w.p2.maxlife
+  var perc1=(w.p1.life*100)/w.p1.maxlife
+  var perc2=(w.p2.life*100)/w.p2.maxlife
   ctx.rect(50,20,middle-100,50)
   ctx.rect(window.innerWidth-50,20,-middle+100,50)
+  ctx.stroke()
   ctx.fillStyle="#FF0000"
   ctx.fillRect(49,21,middle-98,48)
   ctx.fillRect(window.innerWidth-49,21,-middle+98,48)
+  ctx.stroke()
   ctx.fillStyle="#00FF00"
-  ctx.fillRect(middle-50,21,(98-middle)*perc1/100,48)
-  ctx.fillRect(middle+50,21,(middle-98)*perc2/100,48)
-  ctx.fillStyle="white"
+  ctx.fillRect(middle-50,21,(-middle+100)*perc1/100,48)
+  ctx.fillRect(middle+50,21,(middle-100)*perc2/100,48)
+  ctx.stroke()
+  ctx.fillStyle="#AAAAAA"
 
   // Draw names
   ctx.font="30px sans-serif";
@@ -39,4 +53,11 @@ function gui(ctx,w){
   ctx.font="15px sans-serif";
 
   ctx.stroke()
+}
+
+function fight(ctx,w){
+
+  background(ctx);
+  gui(ctx,w)
+
 }
